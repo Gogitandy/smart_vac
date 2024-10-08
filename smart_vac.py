3bc8c87f20495072190c641dc72ef3f38c65f211
Gogitandy <andrew.k.highsmith@gmail.com>
2024-10-08 12:26:15 -0400

    first code add to smart vac

---
+++ smart_vac.py
@@ -0,0 +1,22 @@
+from random import randint
+from room_builder import Room
+
+class SmartVac:
+    def __init__ (self, room):
+        #initialize vac
+        self.room = room
+        self.map = {}
+        self.converted_map = []
+        self.left_sensor = 0
+        self.right_sensor = 0
+        self.front_sensor = 0
+        self.back_sensor = 0
+        self.direction = 0
+        #set vac starting position in random open cell
+        while True:
+            y_p = randint(0, self.room.nrows -1)
+            x_p = randint(0, self.room.ncols -1)
+            if self.room.layout[y_p][x_p]:
+                self.position = (x_p, y_p)
+                break
+        return]
